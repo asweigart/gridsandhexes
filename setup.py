8cbab0899f7fbd0c2ea367545aec2b9068dbf7d6
Al Sweigart <asweigart@gmail.com>
2020-11-09 20:28:17 -0800

    Initial add of project files.

---
+++ setup.py
@@ -0,0 +1,42 @@
+import io
+import os
+import re
+from setuptools import setup, find_packages
+
+scriptFolder = os.path.dirname(os.path.realpath(__file__))
+os.chdir(scriptFolder)
+
+# Find version info from module (without importing the module):
+with open("src/gridsandhexes/__init__.py", "r") as fileObj:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fileObj.read(), re.MULTILINE
+    ).group(1)
+
+# Use the README.md content for the long description:
+with io.open("README.md", encoding="utf-8") as fileObj:
+    long_description = fileObj.read()
+
+setup(
+    name="Grids and Hexes",
+    version=version,
+    url="https://github.com/asweigart/gridsandhexes",
+    author="Al Sweigart",
+    author_email="al@inventwithpython.com",
+    description=("""A Python module for producing PNG and PDF files of customized graph paper."""),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    test_suite="tests",
+    install_requires=[],
+    keywords="",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
---
+++ src/gridsandhexes/__init__.py
@@ -0,0 +1,6 @@
+"""Grids and Hexes
+By Al Sweigart al@inventwithpython.com
+
+A Python module for producing PNG and PDF files of customized graph paper."""
+
+__version__ = '0.1.0'
